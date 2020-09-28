928c80dd5de2478e8e463d59410e076f3a89a942
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2020-09-28 12:39:04 +0300

    Create Aries.py

---
+++ Aries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#x = 1=0
+
+x  ="好**2+龟**2=♈**2=0"
+x1 = "好**2+龟**2=刀**2=0"
+x2 = "People could change their belief system so that they buy cheaper oil"
+x3 = "♍+♍=♍"
+x4 = "♐**2+♈**2=♎**2=1"
+x5 = "we prefer going slow"
+x6 = "I was born for multithreading born to the stars I was born to dream"
+try_to_keep_it_simple = "True"
+do = "or do not there is no try"
+
+print(bool(x))
+print(bool(x1))
+print("False")
+print("False")
+print(bool(x4))
+print(bool(x5))
+print(bool(x6))
+print(bool(try_to_keep_it_simple))
+print("When a.i. is here I am there, when I am there a.i. is here\n"*10)
